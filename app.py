50e024bc727029498ee29868bace9a2b19cf4810
Alldayallan <54728152+Alldayallan@users.noreply.github.com>
2023-12-20 22:43:45 +0000

    added requirements.txt

--- app.py
+++ app.py
@@ -28,9 +28,11 @@ JOBS = [{
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='AllanMan')
 
-@app.route("/jobs")
+
+@app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
